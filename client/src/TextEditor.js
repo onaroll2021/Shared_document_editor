c1b9f6bd503d5fe2db2585193d5549eaae49690f
Ning Li <lining04111223@gmail.com>
2022-11-28 13:18:02 -0800

    add CSS

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,14 +2,39 @@ import { useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+  ["image", "blockquote", "code-block"],
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
+
 export default function TextEditor() {
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
     wrapper.innerHTML = "";
     const editor = document.createElement("div");
     wrapper.append(editor);
-    new Quill(editor, { theme: "snow" });
+    new Quill(editor, {
+      theme: "snow",
+      modules: {
+        toolbar: toolbarOptions,
+      },
+    });
   }, []);
 
-  return <div id="container" ref={wrapperRef}></div>;
+  return <div className="container" ref={wrapperRef}></div>;
 }
