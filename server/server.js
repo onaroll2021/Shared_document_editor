56a9faeee4e5ad86c962d44e3a74dd84b47ab1a6
Ning Li <lining04111223@gmail.com>
2022-11-28 21:51:53 -0800

    add mongo Atlas

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const mongoose = require("mongoose");
-const Document = require("./Document")
+const Document = require("./Document");
 
 const app = express();
 const server = http.createServer(app);
@@ -23,29 +23,28 @@ mongoose
   });
 // socket connect in server
 io.on("connection", (socket) => {
-  socket.on('get-document', async documentId => {
-    const document = await findOrCreateDocument(documentId)
-    socket.join(documentId)
-    socket.emit("load-document", document.data)
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
 
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
-    socket.on("save-document", async data => {
-      await Document.findOneAndUpdate({ URL: documentId }, { data })
-    })
-  })
+    socket.on("save-document", async (data) => {
+      await Document.findOneAndUpdate({ URL: documentId }, { data });
+    });
+  });
 });
 
 //create helper function to retrieve or create document
-const defaultValue = ""
+const defaultValue = "";
 
 async function findOrCreateDocument(URL) {
   if (URL == null) return;
-  const document = await Document.findOne( { URL: URL } )
-  if (document) return document
-  return await Document.create({ URL: URL, data: defaultValue})
+  const document = await Document.findOne({ URL: URL });
+  if (document) return document;
+  return await Document.create({ URL: URL, data: defaultValue });
 }
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
-
