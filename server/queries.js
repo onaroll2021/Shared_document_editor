6b3f5c2c58f18c1d4653565355b564d19a30301d
Ning Li <lining04111223@gmail.com>
2022-11-29 18:09:10 -0800

    add more queries

--- server/queries.js
+++ server/queries.js
@@ -3,14 +3,28 @@ const User = require("./User");
 
 const findDocumentByUserID = (id) => {
   Document.find({ creator: id }).then((data) => {
-
+    console.log(data);
   });
 };
 
 const findUserByID = (id) => {
   User.find({ _id: id }).then((data) => {
+    console.log(data);
   });
 };
+
+const findDocumentByEmail = (email) => {
+  User.find({ email: email }).then((data) => {
+    findDocumentByUserID(data[0]._id);
+  });
+};
+
+//return whole user
+const findUserByEmail = async (email) => {
+  let userarry = await User.find({ email: email });
+  return userarry;
+};
+
 //create new user
 //const user1 = new User({
 //  _id: new mongoose.Types.ObjectId(),
@@ -30,4 +44,9 @@ const findUserByID = (id) => {
 //    console.log(err);
 //  });
 
-module.exports = { findDocumentByUserID, findUserByID };
+module.exports = {
+  findDocumentByUserID,
+  findUserByID,
+  findDocumentByEmail,
+  findUserByEmail,
+};
--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const socketio = require("socket.io");
 const mongoose = require("mongoose");
 const Document = require("./Document");
 const User = require("./User");
-const { findDocumentByUserID, findUserByID } = require("./queries");
-
+const {
+  findDocumentByUserID,
+  findUserByID,
+  findDocumentByEmail,
+  findUserByEmail,
+} = require("./queries");
 
 const app = express();
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
 const io = socketio(server);
@@ -44,29 +48,29 @@ io.on("connection", (socket) => {
 
 const defaultValue = "";
 
-//findOrCreateDocument
 async function findOrCreateDocument(URL) {
+  const findUserarry = await findUserByEmail("lining04111223@gmail.com");
+
   if (URL == null) return;
   const document = await Document.findOne({ URL: URL });
   if (document) return document;
   return await Document.create({
     URL: URL,
     data: defaultValue,
-    creator: "63866ba3d03a71f9898745b8",
+    creator: findUserarry[0]._id,
   });
 }
 
 //select data
-findDocumentByUserID("63866ba3d03a71f9898745b8");
-findUserByID("63866ba3d03a71f9898745b8");
+//findDocumentByUserID("63866ba3d03a71f9898745b8");
+////findUserByID("63866ba3d03a71f9898745b8");
+//findDocumentByEmail("lining04111223@gmail.com");
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
-app.get("/users/dashboard", (req, res) => {
-
-});
+app.get("/users/dashboard", (req, res) => {});
 app.get("/Login");
 app.post("/Login", (req, res) => {
   console.log("req.body", req.body);
-  res.redirect("/users/dashboard")
-  });
+  res.redirect("/users/dashboard");
+});
