ae4a7bfe9754cc7c383d426a8ab0a38041008fa2
Tank-Sun <thomastank0926@gmail.com>
2022-12-04 04:44:10 -0700

    clear input and checkbox after share

--- client/src/components/Header-document.js
+++ client/src/components/Header-document.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { IconButton, Button, Input, Checkbox } from "@material-tailwind/react";
+import { IconButton, Button, Input, Checkbox, Switch } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 
 export default function Documentheader(props) {
@@ -17,21 +17,21 @@ export default function Documentheader(props) {
 
   const handleSend = async () => {
     setSent(true);
+    setShareWithEmail("");
+    setChecked(false);
     const sendFromEmail = props.userEmail;
     const sendToEmail = shareWithEmail;
     const url = props.url;
     const documentUrl = `http://localhost:3000/documents/${props.url}`;
-    try {
-      await axios.post("/api/send_mail", {
+    axios.post("/api/send_mail", {
         sendFromEmail: sendFromEmail,
         sendToEmail: sendToEmail,
         text: documentUrl,
         url: url,
         viewOnly: checked
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    }).catch( (error) => {
+      console.log(error);
+    })
   };
 
   const handleEnterPress = () => {
@@ -93,7 +93,7 @@ export default function Documentheader(props) {
       </div>
 
       <div className="flex flex-end space-x-2">
-        {!sent ? (
+        
           <>
             <form
               className="mx-5 md:mx-10 flex items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md" 
@@ -109,6 +109,7 @@ export default function Documentheader(props) {
               />
               <div className='flex items-center'>
                 <Checkbox
+                  checked={checked}
                   value={checked}
                   onChange={handleChange}
                 />
@@ -117,9 +118,7 @@ export default function Documentheader(props) {
               <Button onClick={handleSend}>Share</Button>
             </form>
           </>
-        ) : (
-          <h1>Email Sent</h1>
-        )}
+
         <img
           loading="lazy"
           className="cursor-pointer h-12 w-12 rounded-full ml-2"
