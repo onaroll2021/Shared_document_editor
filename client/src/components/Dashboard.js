10c6b07e64f6318892014fa1e8ce21565c686e7a
Ning Li <lining04111223@gmail.com>
2022-12-05 08:04:02 -0800

    change little

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -45,7 +45,7 @@ export default function Dashboard() {
   // }) : <></>;
 
   const documentsList = documents.map((document) => {
-    const dateCreated = moment(document.dateTime).startOf("second").fromNow();
+    const dateCreated = moment(document.dateTime).format("DD-MMM-YYYY");
     return (
       <Document
         key={document._id}
@@ -83,10 +83,10 @@ export default function Dashboard() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col">
+    <div>
       <Header />
       <section className="bg-[#F8F9FA] pb-10 px-5">
-        <div className="ml-8">
+        <div className="max-w-3xl mx-auto">
           <div className="flex items-center justify-between py-6">
             <h2 className="text-gray-700 text-lg">Start a new document</h2>
             <button className="button button-icon px-5">
@@ -120,76 +120,75 @@ export default function Dashboard() {
         </div>
       </section>
       <section className="bg-white px-10 md:px-0">
-        <form
-          className="w-1/4 mt-6 ml-72 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md"
-          onSubmit={() => searchResult(documents, search)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            color="gray"
-            size="3xl"
-            className="w-6 h-6"
+        <div className="w-3/5 mx-auto py-8 text-sm text-gray-700">
+          <form
+            className="mx-5 md:mx-10 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md"
+            onSubmit={() => searchResult(documents, search)}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              color="gray"
+              size="3xl"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Search Document"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="flex-grow px-5 text-base bg-transparent outline-none"
             />
-          </svg>
-          <input
-            type="text"
-            placeholder="Search Document"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="flex-grow px-5 text-base bg-transparent outline-none"
-          />
-        </form>
-        <div className="w-3/5 mx-auto py-4 text-sm text-gray-700">
+          </form>
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-100 shadow-md rounded-md">
+            <thead className="bg-gray-200 shadow-md rounded-md">
               <tr>
                 <th
                   scope="col"
-                  className="w-7 py-3 text-xs font-bold text-gray-500 uppercase "
-                >
-                </th>
+                  className="py-3 text-xs font-bold text-gray-500 uppercase "
+                ></th>
                 <th
                   scope="col"
-                  className="w-50 py-3 text-xs font-bold text-left text-gray-700 uppercase "
+                  className="w-50 py-3 text-xs font-bold text-left text-black uppercase "
                 >
                   My Documents
                 </th>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-xs font-bold text-center text-gray-700 uppercase "
+                  className="px-6 py-3 text-xs font-bold text-center text-black uppercase "
                 >
                   Creator
                 </th>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-xs font-bold text-center text-gray-700 uppercase "
+                  className="px-6 py-3 text-xs font-bold text-center text-black uppercase "
                 >
                   Editor
                 </th>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-xs font-bold text-center text-gray-700 uppercase "
+                  className="px-6 py-3 text-xs font-bold text-center text-black uppercase "
                 >
                   Viewer
                 </th>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-xs font-bold text-center text-gray-700 uppercase "
+                  className="px-6 py-3 text-xs font-bold text-center text-black uppercase "
                 >
                   Date Created
                 </th>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-xs font-bold text-center text-gray-700 uppercase "
+                  className="px-6 py-3 text-xs font-bold text-center text-black uppercase "
                 >
                   Delete
                 </th>
@@ -197,31 +196,6 @@ export default function Dashboard() {
             </thead>
             <tbody className="divide-y divide-gray-200">{documentsList}</tbody>
           </table>
-          {/*<p className="mr-12 flex-grow">Title</p>
-            <p className="mr-12">Creator</p>
-            <p className="mr-12">Editor</p>
-            <p className="mr-12">Viewer</p>
-            <p className="mr-12">Date Created</p>
-            <p className="mr-12">Delete</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="gray"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-              />
-            </svg>
-          </div>
-
-          <div>
-            <div className="flex flex-col-reverse">{documentsList}</div>
-          </div>*/}
         </div>
       </section>
     </div>
