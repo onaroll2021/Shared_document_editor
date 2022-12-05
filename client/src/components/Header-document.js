89269f30b894f46c678057aef9204acd8fd8ec9e
Tank-Sun <thomastank0926@gmail.com>
2022-12-04 20:35:48 -0700

    changed style in share part

--- client/src/components/Header-document.js
+++ client/src/components/Header-document.js
@@ -124,13 +124,15 @@ export default function Documentheader(props) {
                 className="mx-5 md:mx-10 flex items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md" 
                 disabled={!canShare()}
               >
-                <Input
-                  label="Share with"
-                  name="share"
-                  type="text"
-                  value={shareWithEmail}
-                  onChange={(event) => setShareWithEmail(event.target.value)}
-                />
+                <div className='w-50'>
+                  <Input
+                    label="Share with"
+                    name="share"
+                    type="text"
+                    value={shareWithEmail}
+                    onChange={(event) => setShareWithEmail(event.target.value)}
+                  />
+                </div>
                 <div className='flex items-center'>
                   <Checkbox
                     checked={checked}
